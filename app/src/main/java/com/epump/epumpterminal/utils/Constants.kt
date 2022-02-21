59263b7681bfb007f90d61c93f6d66aa5317d749
theakanfecode1 <56872396+theakanfecode1@users.noreply.github.com>
2022-02-21 20:04:04 +0100

    Update Constants.kt
    UI updates

--- app/src/main/java/com/epump/epumpterminal/utils/Constants.kt
+++ app/src/main/java/com/epump/epumpterminal/utils/Constants.kt
@@ -1,13 +1,13 @@
 package com.epump.epumpterminal.utils
 
 object Constants {
-    const val BASE_URL = "https://api.epump.com.ng/"
+    const val BASE_URL = "https://api..******..com.ng/"
     const val LOGIN_STATE = "LOGIN_STATE"
     const val PREF_NAME = "EPUMP_PREF"
     const val ACCESS_TOKEN = "ACCESS_TOKEN"
     const val BEARER = "Bearer "
     const val PUMP_EDITED = "PUMP_EDITED"
-    const val PASSWORD = "fuelmetricspassword"
+    const val PASSWORD = ".******."
 
 
 }
